de1382eb51fc8cb6e1b4927b9c2989c8c3aff033
hygorlbm <hygorlbm@gmail.com>
2018-04-20 16:06:44 -0300

    Adicionado paginação

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ class App extends Component {
   constructor(props) {
 		super(props);
 		this.state = {
-			characters: []
-		};
+      characters: [],
+      offset: 0,
+      limit: 4};
 	};
 
 	//Promisecall
@@ -21,23 +22,49 @@ class App extends Component {
 		.then(response => response.json())
 		.then(json => {
 			this.setState({
-				characters: json
+        characters: json,
+        offset: 0,
+        limit: 4
 			});
 		});
   }
 
 //Input filter
 	filter(event){
-		this.setState({filter: event.target.value})
+    this.setState({filter: event.target.value,
+                   offset:0,
+                   limit:4})
   }
   
+  nextPage(){ 
+    this.setState({characters: this.state.characters,
+                   offset: this.state.offset + 4,
+                   limit: this.state.limit + 4 });
+  }
+
+  previousPage(){ 
+    this.setState({characters: this.state.characters,
+                   offset: this.state.offset - 4,
+                   limit: this.state.limit - 4 });
+  }
+
+  isVisible(parameter, sliced_parameter){ 
+    if ((parameter == 0) || (parameter >= sliced_parameter))
+      return {visibility: 'hidden'} ;
+    else
+      return {visibility: 'visible'};
+  }
+
+
+
 
   render() {
-
     let characters = this.state.characters;
+
 		if(this.state.filter){
 			characters = characters.filter( character => character.name.toLowerCase().includes(this.state.filter.toLowerCase()))
     }
+    let sliced = characters.slice(this.state.offset,this.state.limit);
     
     return (
       <div className="App">
@@ -46,20 +73,23 @@ class App extends Component {
           <h1 className="App-title">Challenge 4: Consuming a API (Harry Potter) </h1>
         </header>
         <input className="input" name="search" placeholder="Search a Harry Potter character ..." onChange={this.filter.bind(this)}/>
-				<div className= "every-character">
-          <ul className="characters-list"> {characters.map(character => 
+        <div className= "every-character">
+          <p className="previousPage" style={this.isVisible(this.state.offset, characters.length)} onClick={this.previousPage.bind(this)}><i class="left"></i></p>
+          <ul className="characters-list"> {sliced.map(character => 
             <li key={character.name +" (character-list)"}>
               <CharacterList
-              name={character.name}
-              photo={character.image}
-              house={character.house}
-              actor={character.actor}
-              species={character.species}
-              patronus={character.patronus}
-              alive={character.alive} />
+                name={character.name}
+                photo={character.image}
+                house={character.house}
+                actor={character.actor}
+                patronus={character.patronus}
+                 alive={character.alive} 
+              />
             </li>)}
           </ul>  
-         </div>     
+          <p className="nextPage" style={this.isVisible(this.state.limit, characters.length )} onClick={this.nextPage.bind(this)}><i class="right"></i></p>    
+         </div> 
+         
       </div>
     );
   }
